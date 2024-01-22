dae27044aed06c94309bea896e0f2068b6caa8c5
Berk Cambaz <berkcambaz12321@gmail.com>
2024-01-22 18:25:56 +0300

    Update apiStore.ts

--- web/src/stores/apiStore.ts
+++ web/src/stores/apiStore.ts
@@ -29,10 +29,11 @@ const initialState: ApiStoreState = {
       "ri_agate-B-1": { id: "ri_agate", tier: "B", stars: 1, count: 1 },
       "ri_agate-C-1": { id: "ri_agate", tier: "C", stars: 1, count: 1 },
       "ri_agate-D-1": { id: "ri_agate", tier: "D", stars: 1, count: 1 },
-      "ri_agate-E-1": { id: "ri_agate", tier: "E", stars: 1, count: 1 },
-      "ri_agate-F-1": { id: "ri_agate", tier: "F", stars: 1, count: 1 },
+      "ri_agate-E-1": { id: "ri_agate", tier: "E", stars: 1, count: 5 },
+      "ri_agate-F-5": { id: "ri_agate", tier: "F", stars: 5, count: 3 },
 
-      "ot_monster_scroll-F-0": { id: "ot_monster_scroll", tier: "F", stars: 0, count: 1 },
+      "ot_monster_scroll-F-1": { id: "ot_monster_scroll", tier: "F", stars: 1, count: 5 },
+      "ot_monster_scroll-F-5": { id: "ot_monster_scroll", tier: "F", stars: 5, count: 5 },
     },
     monsters: {
       "angel-1-1": { id: "angel", level: 1, stars: 1, time: 1 },
